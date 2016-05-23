2aa428d59500d331cae72bae501463f2806f6e9b
Ankur Rastogi <ankur.rastogi@paytm.com>
2016-05-23 17:36:04 +0530

    added BFS and DFS traversa Algos

---
+++ Graph/BFSTraversal.js
@@ -0,0 +1,56 @@
+"use strict";
+
+//Internal Modules
+var Queue = require("../Queue/Queue")
+
+//Node Gloabl Modules
+var util = require("util");
+
+var BFSTraversal = function(graph){
+	this.traversedVertices = [];
+	this.queue = new Queue();
+	this.graph = graph;
+};
+
+/** * Algo For BFSTraversal
+ * 
+ * Start from the starting vertices passed in 
+ * Put that vertices in traversedVertices Array
+ * Find all the adjacent vertices of that vertices and enqueue them in a queue.
+ * 		* Dequeue a vertice from queue
+ *		* Push this vertice in traversedVertices Array.
+ *		* Find all its adjacent vertices by traversing its adjacencey list
+ *		* Enqueue all the adjacent vertices in queue.
+ *		* Repeat all above steps untill all vertices a traversed
+ */
+
+
+BFSTraversal.prototype.stratTraversing = function(vertice){
+	if(!this.graph || !this.graph.adjArray || !this.graph.adjArray[vertice]) {
+		return util.log("Graph is not having the desired vertices.");
+	}
+
+	//Push first element in the group
+	this.queue.enqueue(vertice);
+	this.traversedVertices.push(vertice);
+
+	//While look till the queue gets empty
+	while(!this.queue.isEmpty()){
+		
+		var item = this.queue.dequeue();
+		console.log(item);
+		
+		var n = this.graph.adjArray[item].head;
+		
+		while(n !== null){
+			if(n.value != null && this.traversedVertices.indexOf(n.value) < 0){
+				this.queue.enqueue(n.value);
+				this.traversedVertices.push(n.value);
+			}
+			n = n.next;
+		}
+	}
+};
+
+module.exports = BFSTraversal;
+
--- test/Graph.js
+++ test/Graph.js
@@ -1,23 +1,37 @@
 "use strict";
 
 var Graph = require("../Graph");
+var BFSTraversal = require("../Graph/BFSTraversal");
 var graph;
 
 describe("#Graph Undirected - Basic Operations", function(){
 	
 	before("#Graph Undirected - init", function(){
-		graph = new Graph(5);
+		// graph = new Graph(5);
+		// graph.addEdge(0, 1);
+	 //    graph.addEdge(0, 4);
+	 //    graph.addEdge(1, 2);
+	 //    graph.addEdge(1, 3);
+	 //    graph.addEdge(1, 4);
+	 //    graph.addEdge(2, 3);
+	 //    graph.addEdge(3, 4);
+	 	graph = new Graph(4);
+		graph.addEdge(0, 1);
+	    graph.addEdge(0, 2);
+	    graph.addEdge(1, 2);
+	    graph.addEdge(2, 0);
+	    graph.addEdge(2, 3);
+	    graph.addEdge(3, 3);
 	});
 
 	it("#Graph Undirected - print", function(done){
-		graph.addEdge(0, 1);
-	    graph.addEdge(0, 4);
-	    graph.addEdge(1, 2);
-	    graph.addEdge(1, 3);
-	    graph.addEdge(1, 4);
-	    graph.addEdge(2, 3);
-	    graph.addEdge(3, 4);
 		graph.print();
+		done();
+	});
+
+	it("#Graph BFSTraversal", function(done){
+		var bfsTraversal = new BFSTraversal(graph);
+		bfsTraversal.stratTraversing(2);
 		done();
 	});
 });
