2923e152a21aef3e425180afa207dbd7a77bc3e9
Ankur Rastogi <ankurrastogi09@gmail.com>
2016-06-19 00:10:05 +0530

    added a method in Btree

--- BSTree/index.js
+++ BSTree/index.js
@@ -8,10 +8,15 @@ var BSTree = function(){
 	this.dfsTraversal = new DFSTraversal();
 }
 
+//Method to insert a node in binary search tree
 BSTree.prototype.insert = function(value) {
 
+	//create a node with given value
 	var n = new Node(value);
 
+	//check if root is null 
+	//then assign new node to root 
+	//else call iterate method
 	if(this.root === null){
 		this.root = n;
 	} else {
@@ -20,8 +25,14 @@ BSTree.prototype.insert = function(value) {
 
 	function iterate(node){
 		
+		//check if node value to be added is less 
+		//than the traversed node value
+		//else check for right node
 		if(value < node.value) {
-		
+			
+			//if left child of node is null 
+			//than assign new node to left child
+			//else call iterate method
 			if(node.left === null){
 				node.left = n;
 				return;
@@ -46,23 +57,30 @@ BSTree.prototype.insert = function(value) {
 	}
 };
 
+
+//Methods to search in a binary tree
 BSTree.prototype.search = function(value){
 
 	var searchedNode = null;
 
+	//Call iterate method by passing root in it.
 	iterate(this.root, value);
 
 	function iterate(node, value){
+		//check if node is null.
 		if(node === null || node === undefined){
 			return null;
 		}
 
+		//Check if node's value is equal to the value we are searching for.
 		if(node.value === value){
 			searchedNode = node;
 		}
 
+		//Move to the left child if value is less thn node's value
 		if(value < node.value){
 			iterate(node.left, value);
+		//Move to the right if the value is more than the node's value
 		} else if(value > node.value) {
 			iterate(node.right, value);
 		}
